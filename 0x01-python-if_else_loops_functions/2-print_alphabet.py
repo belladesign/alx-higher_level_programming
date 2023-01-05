cf1e68f4eefd7faba223e7ef715e33ebdd8e4431
belladesign <taliaowen7@gmail.com>
2023-01-05 01:14:37 -0800

    fixing

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch in range(ord{'a'},ord{'z'} + 1);
-        print("{ }".format(chr(ch)), end='')
+for ch in range(ord{97, 123);
+        print("{}".format(chr(ch)), end='"')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
-        if ch != 101 and ch != 113:
-                print("{:c}".format(ch), end='')
+        if chr(ch) != 'q' and chr(ch) != 'e':
+                print("{}".format(chr(ch)), end='')
