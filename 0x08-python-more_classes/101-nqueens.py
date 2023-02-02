e85bdde69d306948009778674858b148bca14f9d
belladesign <81829917+belladesign@users.noreply.github.com>
2023-02-02 10:50:31 +0100

    fix

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -96,11 +96,11 @@ if __name__ == '__main__':
 
     try:
         size = int(sys.argv[1])
-    except:
+    except Exception:
         print("N must be a number")
         sys.exit(1)
 
-    if size < 4:
+    if number < 4:
         print("N must be at least 4")
         sys.exit(1)
 
