674f022838093ff5ddf5ed6d995497939f676c82
belladesign <81829917+belladesign@users.noreply.github.com>
2023-04-18 15:43:23 +0100

    Update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
@@ -7,3 +6,5 @@ module.exports = class Square extends Rectangle {
     super(size, size);
   }
 };
+
+module.exports = Square
