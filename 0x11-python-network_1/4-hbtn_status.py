f5023bfb3a675118d81ea6848fb2d32214909d44
belladesign <81829917+belladesign@users.noreply.github.com>
2023-04-28 17:18:07 +0100

    fixing 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+""" now we are going to use requests package
 """
-Python script that fetches an URL with requests package
-"""
-import requests
-
-
-if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+if __name__ == '__main__':
+    import requests
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    typ = type(r.text)
+    con = r.text
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(typ, con))
