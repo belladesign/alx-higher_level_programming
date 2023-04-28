f7faacc44508b7217604835f2f5acb62330085ed
belladesign <81829917+belladesign@users.noreply.github.com>
2023-04-28 17:51:15 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-""" json with requests package
 """
+Python script that sends a POST request to the URL and
+to an URL with the letter as a parameter
+"""
+import requests
+import sys
+
 
 if __name__ == "__main__":
-    import requests
-    import sys
+    data = {'q': ""}
 
-    value = {'q': q=""}
-
-    if (len(sys.argv) > 1):
-        value['q'] = sys.argv[1]
-
-    r = requests.post("http://0.0.0.0:5000/search_user", data=value)
     try:
-        js = r.json()
-        if len(js) == 0:
+        data['q'] = sys.argv[1]
+    except:
+        pass
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data)
+
+    try:
+        json_o = r.json()
+        if not json_o:
             print("No result")
         else:
-            print("[{}] {}".format(js.get('id'), js.get('name')))
+            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
     except:
         print("Not a valid JSON")
