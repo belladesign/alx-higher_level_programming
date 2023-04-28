123d08d245c33333ea6160ba5eea033e7c4ccf2b
belladesign <81829917+belladesign@users.noreply.github.com>
2023-04-28 18:54:36 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" get the first 10 commits from repo
 """
-if __name__ == "__main__":
-    import sys
-    import requests
+Python script that shows the last 10 commits of a repository
+in GitHub
+"""
+from requests import get, auth
+import sys
 
-    owner = sys.argv[2]
-    repo = sys.argv[1]
-    API = "https://developer.github.com/v3/repos/commits/".format(owner, repo)
-    r = requests.get(API)
-    # the commits are in order from new to old
-    for i in range(len(r.json())):
-        if (i < 10):
-            commit = r.json()[i]
-            sha = commit.get('sha')
-            author = commit.get('commit').get('author').get('name')
-            print("{}: {}".format(sha, author))
+
+if __name__ == "__main__":
+    try:
+        repo = sys.argv[1]
+        owner = sys.argv[2]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+        r = get(url)
+        json_o = r.json()
+        for i in range(0, 10):
+            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
+                                  .get('author').get('name')))
+    except:
+        pass
