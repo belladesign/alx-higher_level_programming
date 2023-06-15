d4bb58274cfa44b4f95a8103d4b20b097329a24f
belladesign <81829917+belladesign@users.noreply.github.com>
2023-06-15 12:46:19 +0100

    Correcting 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,32 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-const args = process.argv.slice(2);
-
-function userTaskCompleted (apiUrl) {
-  request.get(apiUrl, function (error, response, body) {
-    if (error) {
-      console.log(error);
-    } else {
-      const json = JSON.parse(body);
-      const completed = {};
-      for (const task of json) {
-        if (task.completed === true) {
-          /* if a new key with no value */
-          if (completed[task.userId] === undefined) {
-            completed[task.userId] = 1;
-            /* if key exists incremen the value of key */
-          } else {
-            completed[task.userId]++;
-          }
-        }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-      console.log(completed);
-    }
-  });
-}
-
-if (args.length === 1) {
-  const apiUrl = args[0];
-  userTaskCompleted(apiUrl);
-}
+    });
+    console.log(completed);
+  }
+});
